30a76cd2e5644dcf516d312afb30ca97b77788fb
ectionboy <ection.boy@gmail.com>
2023-09-06 20:43:35 +0300

    v1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,11 @@
-export const App = () => {
+import { Card } from "./Profile/Profile";
+import user from '../data/user.json'
+
+
+const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+<>
+
+<Card /></>
   );
 };
